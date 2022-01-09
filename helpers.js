b4bbbbe9f6a1e5ea4da29092de542063b99ea6e6
ABUOP1 <94583201+ABUOP1@users.noreply.github.com>
2022-01-09 14:12:05 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Maalutty*:  ```" + msg + "```"
+    return "✅ *Abuser*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Maalutty*:  ```" + msg + "```"
+    return "🛑 *Abuser*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Maalutty*:  ```" + msg + "```"
+    return "⏺️ *Abuser*:  ```" + msg + "```"
 }
 
 
